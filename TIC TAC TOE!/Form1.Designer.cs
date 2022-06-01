49d09a4a89ac34bc2fde146c99e8a5c938c30429
Adrian Arcega <adrianarcega18@gmail.com>
2022-06-02 00:19:08 +0800

    Buttons and design added

--- TIC TAC TOE!/Form1.Designer.cs
+++ TIC TAC TOE!/Form1.Designer.cs
@@ -28,13 +28,122 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button_1 = new System.Windows.Forms.Button();
+            this.button_2 = new System.Windows.Forms.Button();
+            this.button_3 = new System.Windows.Forms.Button();
+            this.button_6 = new System.Windows.Forms.Button();
+            this.button_5 = new System.Windows.Forms.Button();
+            this.button_4 = new System.Windows.Forms.Button();
+            this.button_9 = new System.Windows.Forms.Button();
+            this.button_8 = new System.Windows.Forms.Button();
+            this.button_7 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button_1
+            // 
+            resources.ApplyResources(this.button_1, "button_1");
+            this.button_1.Name = "button_1";
+            this.button_1.UseVisualStyleBackColor = true;
+            // 
+            // button_2
+            // 
+            resources.ApplyResources(this.button_2, "button_2");
+            this.button_2.Name = "button_2";
+            this.button_2.UseVisualStyleBackColor = true;
+            // 
+            // button_3
+            // 
+            resources.ApplyResources(this.button_3, "button_3");
+            this.button_3.Name = "button_3";
+            this.button_3.UseVisualStyleBackColor = true;
+            // 
+            // button_6
+            // 
+            resources.ApplyResources(this.button_6, "button_6");
+            this.button_6.Name = "button_6";
+            this.button_6.UseVisualStyleBackColor = true;
+            // 
+            // button_5
+            // 
+            resources.ApplyResources(this.button_5, "button_5");
+            this.button_5.Name = "button_5";
+            this.button_5.UseVisualStyleBackColor = true;
+            // 
+            // button_4
+            // 
+            resources.ApplyResources(this.button_4, "button_4");
+            this.button_4.Name = "button_4";
+            this.button_4.UseVisualStyleBackColor = true;
+            // 
+            // button_9
+            // 
+            resources.ApplyResources(this.button_9, "button_9");
+            this.button_9.Name = "button_9";
+            this.button_9.UseVisualStyleBackColor = true;
+            // 
+            // button_8
+            // 
+            resources.ApplyResources(this.button_8, "button_8");
+            this.button_8.Name = "button_8";
+            this.button_8.UseVisualStyleBackColor = true;
+            // 
+            // button_7
+            // 
+            resources.ApplyResources(this.button_7, "button_7");
+            this.button_7.Name = "button_7";
+            this.button_7.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackgroundImage = global::TIC_TAC_TOE_.Properties.Resources._01073865290819_5d61d475f0072;
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button_9);
+            this.Controls.Add(this.button_8);
+            this.Controls.Add(this.button_7);
+            this.Controls.Add(this.button_6);
+            this.Controls.Add(this.button_5);
+            this.Controls.Add(this.button_4);
+            this.Controls.Add(this.button_3);
+            this.Controls.Add(this.button_2);
+            this.Controls.Add(this.button_1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button_1;
+        private System.Windows.Forms.Button button_2;
+        private System.Windows.Forms.Button button_3;
+        private System.Windows.Forms.Button button_6;
+        private System.Windows.Forms.Button button_5;
+        private System.Windows.Forms.Button button_4;
+        private System.Windows.Forms.Button button_9;
+        private System.Windows.Forms.Button button_8;
+        private System.Windows.Forms.Button button_7;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
     }
 }
 
