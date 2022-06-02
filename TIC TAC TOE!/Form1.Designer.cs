684b451e5315f5a13cd4fd1786c8f083a8c6f261
Adrian Arcega <adrianarcega18@gmail.com>
2022-06-02 18:07:23 +0800

    X and O (inserted)

--- TIC TAC TOE!/Form1.Designer.cs
+++ TIC TAC TOE!/Form1.Designer.cs
@@ -47,63 +47,75 @@
             resources.ApplyResources(this.button_1, "button_1");
             this.button_1.Name = "button_1";
             this.button_1.UseVisualStyleBackColor = true;
+            this.button_1.Click += new System.EventHandler(this.button_clicked);
             // 
             // button_2
             // 
             resources.ApplyResources(this.button_2, "button_2");
             this.button_2.Name = "button_2";
             this.button_2.UseVisualStyleBackColor = true;
+            this.button_2.Click += new System.EventHandler(this.button_clicked);
             // 
             // button_3
             // 
             resources.ApplyResources(this.button_3, "button_3");
             this.button_3.Name = "button_3";
             this.button_3.UseVisualStyleBackColor = true;
+            this.button_3.Click += new System.EventHandler(this.button_clicked);
             // 
             // button_6
             // 
             resources.ApplyResources(this.button_6, "button_6");
             this.button_6.Name = "button_6";
             this.button_6.UseVisualStyleBackColor = true;
+            this.button_6.Click += new System.EventHandler(this.button_clicked);
             // 
             // button_5
             // 
             resources.ApplyResources(this.button_5, "button_5");
             this.button_5.Name = "button_5";
             this.button_5.UseVisualStyleBackColor = true;
+            this.button_5.Click += new System.EventHandler(this.button_clicked);
             // 
             // button_4
             // 
             resources.ApplyResources(this.button_4, "button_4");
             this.button_4.Name = "button_4";
             this.button_4.UseVisualStyleBackColor = true;
+            this.button_4.Click += new System.EventHandler(this.button_clicked);
             // 
             // button_9
             // 
             resources.ApplyResources(this.button_9, "button_9");
             this.button_9.Name = "button_9";
             this.button_9.UseVisualStyleBackColor = true;
+            this.button_9.Click += new System.EventHandler(this.button_clicked);
             // 
             // button_8
             // 
             resources.ApplyResources(this.button_8, "button_8");
             this.button_8.Name = "button_8";
             this.button_8.UseVisualStyleBackColor = true;
+            this.button_8.Click += new System.EventHandler(this.button_clicked);
             // 
             // button_7
             // 
             resources.ApplyResources(this.button_7, "button_7");
             this.button_7.Name = "button_7";
             this.button_7.UseVisualStyleBackColor = true;
+            this.button_7.Click += new System.EventHandler(this.button_clicked);
             // 
             // button2
             // 
             resources.ApplyResources(this.button2, "button2");
+            this.button2.Image = global::TIC_TAC_TOE_.Properties.Resources.ori_3519461_7f53d389c070c12236a18e13693a1c9b1292b001_pixel_8bit_game_vector_background_with_ground_and_cloudy_sky_panorama;
             this.button2.Name = "button2";
             this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
             // button1
             // 
+            this.button1.BackgroundImage = global::TIC_TAC_TOE_.Properties.Resources.ori_3519461_7f53d389c070c12236a18e13693a1c9b1292b001_pixel_8bit_game_vector_background_with_ground_and_cloudy_sky_panorama;
             resources.ApplyResources(this.button1, "button1");
             this.button1.Name = "button1";
             this.button1.UseVisualStyleBackColor = true;
--- TIC TAC TOE!/Form1.cs
+++ TIC TAC TOE!/Form1.cs
@@ -12,9 +12,24 @@ namespace TIC_TAC_TOE_
 {
     public partial class Form1 : Form
     {
+        bool turn = true; //true = A turn; false = B turn
+        int turn_count = 0;
         public Form1()
         {
             InitializeComponent();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+        private void button_clicked(object sender, EventArgs e)
+        {
+            Button x = (Button)sender;
+            if (turn)
+                x.Text = "X";
+            else
+                x.Text = "O";
+            turn = !turn;
         }
     }
 }
